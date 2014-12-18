2182946f0da83e9ed5ca1e0628a438a5b5d594e5
Jack Sleight <jacksleight@gmail.com>
2014-12-18 19:19:50 +0000

    Support additional caching drivers.

--- app/cache.php
+++ app/cache.php
@@ -1,18 +1,38 @@
 <?php
 use Doctrine\Common\Cache\ArrayCache,
-	Doctrine\Common\Cache\MemcachedCache;
+	Doctrine\Common\Cache\MemcacheCache,
+	Doctrine\Common\Cache\MemcachedCache,
+	Doctrine\Common\Cache\RedisCache,
+	Doctrine\Common\Cache\XcacheCache;
 
-if ($app->isDevelopment()) {
-	$cache = new ArrayCache();
-} else {
-	if (isset($app->config->memcached) && extension_loaded('memcached')) {
-		$memcached = new Memcached();
-		$memcached->addServer($app->config->memcached['host'], $app->config->memcached['port']);
-		$cache = new MemcachedCache();
-		$cache->setMemcached($memcached);
-	} else {
-		$cache = new ArrayCache();
-	}
+if ($app->isDevelopment() || !isset($app->config->cache)) {
+	return new ArrayCache();
 }
 
-return $cache;
+$config = $app->config->cache;
+if ($config['driver'] == 'memcache' && extension_loaded('memcache')) {
+	$memcache = new Memcache();
+	$memcache->connect($config['host'], $config['port']);
+	$cache = new MemcacheCache();
+	$cache->setMemcache($memcache);
+	return $cache;
+}
+if ($config['driver'] == 'memcached' && extension_loaded('memcached')) {
+	$memcached = new Memcached();
+	$memcached->addServer($config['host'], $config['port']);
+	$cache = new MemcachedCache();
+	$cache->setMemcached($memcached);
+	return $cache;
+}
+if ($config['driver'] == 'redis' && extension_loaded('redis')) {
+	$redis = new Redis();
+	$redis->connect($config['host'], $config['port']);
+	$cache = new RedisCache();
+	$cache->setRedis($redis);
+	return $cache;
+}
+if ($config['driver'] == 'xcache' && extension_loaded('xcache')) {
+	$cache = new XcacheCache();
+	return $cache;
+}
+return new ArrayCache();
