687d41d35766caf2528c9273aa23630e232e97d7
Jack Sleight <jacksleight@gmail.com>
2015-01-15 15:27:47 +0000

    Sandbox view for testing.

---
+++ app/views/index/sandbox.php
@@ -0,0 +1,53 @@
+<?php if (!$req->isAjax()) { ?>
+	<?php $this->parent('/layout/page_content') ?>
+	<?php $this->block('main') ?>
+<?php } ?>
+
+<p>
+	<input class="btn" type="button" value="Button">
+	<button class="btn">Button</button>
+	<a class="btn">Button</a>
+	<span class="btn">Button</span>
+</p>
+<p>
+	<input class="btn btn-focus" type="button" value="Button">
+	<button class="btn btn-focus">Button</button>
+	<a class="btn btn-focus">Button</a>
+	<span class="btn btn-focus">Button</span>
+</p>
+<p>
+	<input class="btn btn-positive" type="button" value="Button">
+	<button class="btn btn-positive">Button</button>
+	<a class="btn btn-positive">Button</a>
+	<span class="btn btn-positive">Button</span>
+</p>
+<p>
+	<input class="btn btn-negative" type="button" value="Button">
+	<button class="btn btn-negative">Button</button>
+	<a class="btn btn-negative">Button</a>
+	<span class="btn btn-negative">Button</span>
+</p>
+<p>
+	<input class="btn btn-primary" type="button" value="Button">
+	<button class="btn btn-primary">Button</button>
+	<a class="btn btn-primary">Button</a>
+	<span class="btn btn-primary">Button</span>
+</p>
+<p>
+	<input class="btn btn-primary btn-focus" type="button" value="Button">
+	<button class="btn btn-primary btn-focus">Button</button>
+	<a class="btn btn-primary btn-focus">Button</a>
+	<span class="btn btn-primary btn-focus">Button</span>
+</p>
+<p>
+	<input class="btn btn-primary btn-positive" type="button" value="Button">
+	<button class="btn btn-primary btn-positive">Button</button>
+	<a class="btn btn-primary btn-positive">Button</a>
+	<span class="btn btn-primary btn-positive">Button</span>
+</p>
+<p>
+	<input class="btn btn-primary btn-negative" type="button" value="Button">
+	<button class="btn btn-primary btn-negative">Button</button>
+	<a class="btn btn-primary btn-negative">Button</a>
+	<span class="btn btn-primary btn-negative">Button</span>
+</p>
