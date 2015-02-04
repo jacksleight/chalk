93cc9dfd84798e7ac4a42ae6ef9a74f067ff235b
Jack Sleight <jacksleight@gmail.com>
2015-02-04 11:23:38 +0000

    Fix error in secondary structures.

--- app/views/structure/node/edit.php
+++ app/views/structure/node/edit.php
@@ -1,13 +1,16 @@
-<?php
-$structure	= $this->em('Chalk\Core\Structure')->id($req->structure);
-$content	= $node->content;
-$info	    = \Chalk\Chalk::info($content->getObject());
-?>
-
 <?php $this->parent('/layout/page_structure', [
 	'content' => $content,
 ]) ?>
 <?php $this->block('main') ?>
+
+<?php
+if (!isset($node->content)) {
+	return;
+}
+$structure	= $this->em('Chalk\Core\Structure')->id($req->structure);
+$content	= $node->content;
+$info	    = \Chalk\Chalk::info($content->getObject());
+?>
 
 <?php if ($info->class != 'Chalk\Core\Content') { ?>
 	<form action="<?= $this->url->route() ?>" method="post">
