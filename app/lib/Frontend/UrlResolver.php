baa5395b50525075ab2b4a973e7dcafb9b3c1d31
Jack Sleight <jacksleight@gmail.com>
2015-02-06 16:59:09 +0000

    Tweak url resolver.

--- app/lib/Frontend/UrlResolver.php
+++ app/lib/Frontend/UrlResolver.php
@@ -13,7 +13,7 @@ class UrlResolver extends CoastUrlResolver
     public function __invoke()
     {
         $args = func_get_args();
-        if (isset($args[0]) && is_numeric()) {
+        if (isset($args[0]) && is_numeric($args[0])) {
             $func = array($this, 'content');
         } else {
             $func = array('parent', '__invoke');
