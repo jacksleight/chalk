13d0b6e1a29c4a2ded087149dce81905c7108537
Jack Sleight <jacksleight@gmail.com>
2015-02-25 18:34:30 +0000

    Include additional date filters in index model.

--- app/lib/Core/Model/Index.php
+++ app/lib/Core/Model/Index.php
@@ -12,7 +12,12 @@ class Index extends \Toast\Entity
 	protected $limit = 50;
 	protected $sort;
 	protected $search;
+	protected $createDateMin;
+	protected $createDateMax;
 	protected $modifyDateMin;
+	protected $modifyDateMax;
+	protected $publishDateMin;
+	protected $publishDateMax;
 	protected $statuses = [
 		\Chalk\Chalk::STATUS_DRAFT,
 		\Chalk\Chalk::STATUS_PUBLISHED,
@@ -50,6 +55,26 @@ class Index extends \Toast\Entity
 					'type'		=> 'string',
 					'nullable'	=> true,
 				),
+				'createDateMin' => array(
+					'type'		=> 'string',
+					'nullable'	=> true,
+					'values'	=> [
+						'today'		=> 'Today',
+						'-1 hour'	=> 'Past Hour',
+						'-1 day'	=> 'Past Day',
+						'-2 day'	=> 'Past 2 Days',
+						'-1 week'	=> 'Past Week',
+						'-2 week'	=> 'Past 2 Weeks',
+						'-1 month'	=> 'Past Month',
+						'-3 month'	=> 'Past 3 Months',
+						'-6 month'	=> 'Past 6 Months',
+						'-1 year'	=> 'Past Year',
+					]
+				),
+				'createDateMax' => array(
+					'type'		=> 'string',
+					'nullable'	=> true,
+				),
 				'modifyDateMin' => array(
 					'type'		=> 'string',
 					'nullable'	=> true,
@@ -65,6 +90,30 @@ class Index extends \Toast\Entity
 						'-6 month'	=> 'Past 6 Months',
 						'-1 year'	=> 'Past Year',
 					]
+				),
+				'modifyDateMax' => array(
+					'type'		=> 'string',
+					'nullable'	=> true,
+				),
+				'publishDateMin' => array(
+					'type'		=> 'string',
+					'nullable'	=> true,
+					'values'	=> [
+						'today'		=> 'Today',
+						'-1 hour'	=> 'Past Hour',
+						'-1 day'	=> 'Past Day',
+						'-2 day'	=> 'Past 2 Days',
+						'-1 week'	=> 'Past Week',
+						'-2 week'	=> 'Past 2 Weeks',
+						'-1 month'	=> 'Past Month',
+						'-3 month'	=> 'Past 3 Months',
+						'-6 month'	=> 'Past 6 Months',
+						'-1 year'	=> 'Past Year',
+					]
+				),
+				'publishDateMax' => array(
+					'type'		=> 'string',
+					'nullable'	=> true,
 				),
 				'statuses' => array(
 					'type'		=> 'array',
