d46720f1ea8c71580a30b1c616386961eae07de7
Jack Sleight <jacksleight@gmail.com>
2015-04-01 18:34:13 +0100

    Optimise alias fetch.

---
+++ app/lib/Core/Repository/Alias.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Copyright 2015 Jack Sleight <http://jacksleight.com/>
+ * This source file is subject to the MIT license that is bundled with this package in the file LICENCE. 
+ */
+
+namespace Chalk\Core\Repository;
+
+use Chalk\Repository;
+
+class Alias extends Content
+{
+    public function query(array $criteria = array(), $sort = null, $limit = null, $offset = null)
+    {
+        $query = parent::query($criteria, $sort, $limit, $offset);
+
+        $query
+            ->addSelect("cc")
+            ->leftJoin("c.content", "cc");
+
+        return $query;
+    }
+}
