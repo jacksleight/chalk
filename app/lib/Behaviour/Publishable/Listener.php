ed47f623ffc1cbf09ecea9d03e26a78662ba3e1a
Jack Sleight <jacksleight@gmail.com>
2015-10-01 20:28:01 +0100

    Update license file

--- app/lib/Behaviour/Publishable/Listener.php
+++ app/lib/Behaviour/Publishable/Listener.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Copyright 2015 Jack Sleight <http://jacksleight.com/>
- * This source file is subject to the MIT license that is bundled with this package in the file LICENCE. 
+ * This source file is subject to the MIT license that is bundled with this package in the file LICENCE.md. 
  */
 
 namespace Chalk\Behaviour\Publishable;
