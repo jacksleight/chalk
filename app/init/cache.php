1e27b37c3c4f19aa131da1dee339d706bd7c8e4d
Jack Sleight <jacksleight@gmail.com>
2015-10-05 14:14:48 +0100

    Ability to specify cache namespace.

--- app/init/cache.php
+++ app/init/cache.php
@@ -10,34 +10,29 @@ use Doctrine\Common\Cache\ArrayCache,
 	Doctrine\Common\Cache\RedisCache,
 	Doctrine\Common\Cache\XcacheCache;
 
-if ($app->isDevelopment() || !isset($app->config->cache)) {
-	return new ArrayCache();
-}
-
 $config = $app->config->cache;
-if ($config['driver'] == 'memcache' && extension_loaded('memcache')) {
+if ($app->isDevelopment()) {
+	$cache = new ArrayCache();
+} else if ($config['driver'] == 'memcache' && extension_loaded('memcache')) {
 	$memcache = new Memcache();
 	$memcache->connect($config['host'], $config['port']);
 	$cache = new MemcacheCache();
 	$cache->setMemcache($memcache);
-	return $cache;
-}
-if ($config['driver'] == 'memcached' && extension_loaded('memcached')) {
+} else if ($config['driver'] == 'memcached' && extension_loaded('memcached')) {
 	$memcached = new Memcached();
 	$memcached->addServer($config['host'], $config['port']);
 	$cache = new MemcachedCache();
 	$cache->setMemcached($memcached);
-	return $cache;
-}
-if ($config['driver'] == 'redis' && extension_loaded('redis')) {
+} else if ($config['driver'] == 'redis' && extension_loaded('redis')) {
 	$redis = new Redis();
 	$redis->connect($config['host'], $config['port']);
 	$cache = new RedisCache();
 	$cache->setRedis($redis);
-	return $cache;
-}
-if ($config['driver'] == 'xcache' && extension_loaded('xcache')) {
+} else if ($config['driver'] == 'xcache' && extension_loaded('xcache')) {
 	$cache = new XcacheCache();
-	return $cache;
+} else {
+	$cache = new ArrayCache();
 }
-return new ArrayCache();
+$cache->setNamespace($config['namespace']);
+
+return $cache;
