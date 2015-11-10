ca67ec4f6434f6334bcbece5092ba461e4a31ec5
Jack Sleight <jacksleight@gmail.com>
2015-11-10 12:53:54 +0000

    Fix input content value variable.

--- modules/core/backend/views/element/input_content.php
+++ modules/core/backend/views/element/input_content.php
@@ -5,6 +5,9 @@ if (!isset($filters)) {
 		Chalk::info($md['entity'])->name => true,
 	];
 }
+$value = isset($value)
+	? $value
+	: null;
 ?>
 <div class="input-content" data-params="<?= $this->escape(http_build_query(['filters' => $filters])) ?>">
 	<div class="input-content-controls">
@@ -12,17 +15,15 @@ if (!isset($filters)) {
 		<span class="input-content-select btn btn-lighter btn-icon icon-browse"><span>Browse</span></span>
 	</div>
 	<div class="input-content-holder">
-		<?php if (isset($value)) { ?>
-			<?php
-			$content = $this->em('Chalk\Core\Content')->id($value);
-			?>
-			<?php if ($content) { ?>
-				<?= $this->inner('/content/card', [
-					'content' => $content
-				]) ?>		
-			<?php } else { ?>
-				<span class="placeholder">Nothing Selected</span>
-			<?php } ?>
+		<?php
+		$content = isset($value)
+			? $this->em('Chalk\Core\Content')->id($value)
+			: null;
+		?>
+		<?php if ($content) { ?>
+			<?= $this->inner('/content/card', [
+				'content' => $content
+			]) ?>		
 		<?php } else { ?>
 			<span class="placeholder">Nothing Selected</span>
 		<?php } ?>
