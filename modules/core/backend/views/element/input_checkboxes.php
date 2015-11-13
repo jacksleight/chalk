cf10439cf24109b5e713c93d9bf61e246b13525a
Jack Sleight <jacksleight@gmail.com>
2015-11-13 14:45:02 +0000

    Tweak checkboxes styling

--- modules/core/backend/views/element/input_checkboxes.php
+++ modules/core/backend/views/element/input_checkboxes.php
@@ -1,20 +1,22 @@
-<?php foreach ($values as $v => $l) { ?>
-	<span style="display: inline-block; width: 49%;">	
-		<input
-			type="hidden"
-			name="<?= "{$name}[{$this->escape($v)}]" ?>"
-			value="0">
-		<input
-			type="checkbox"
-			name="<?= "{$name}[{$this->escape($v)}]" ?>"
-			id="<?= "{$id}[{$this->escape($v)}]" ?>"
-			value="1"
-			<?= isset($disabled) && $disabled ? "disabled" : null ?>
-			<?= isset($readOnly) && $readOnly ? "readonly" : null ?>
-			<?= in_array($v, $value) ? 'checked' : null ?>
-			<?= isset($class) ? "class=\"{$class}\"" : null ?>> 
-		<label for="<?= "{$id}[{$this->escape($v)}]" ?>" class="checkbox">
-			<?= $l ?>
-		</label>
-	</span>
-<?php } ?>
+<ul class="checkboxes">
+	<?php foreach ($values as $v => $l) { ?>
+		<li class="checkboxes_i">
+			<input
+				type="hidden"
+				name="<?= "{$name}[{$this->escape($v)}]" ?>"
+				value="0">
+			<input
+				type="checkbox"
+				name="<?= "{$name}[{$this->escape($v)}]" ?>"
+				id="<?= "{$id}[{$this->escape($v)}]" ?>"
+				value="1"
+				<?= isset($disabled) && $disabled ? "disabled" : null ?>
+				<?= isset($readOnly) && $readOnly ? "readonly" : null ?>
+				<?= in_array($v, $value) ? 'checked' : null ?>
+				<?= isset($class) ? "class=\"{$class}\"" : null ?>> 
+			<label for="<?= "{$id}[{$this->escape($v)}]" ?>" class="checkbox">
+				<?= $l ?>
+			</label>
+		</li>
+	<?php } ?>
+</ul>
