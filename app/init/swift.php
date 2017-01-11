bac0a742724b17695dbbe63dc853e6721c957ebf
Jack Sleight <jacksleight@gmail.com>
2017-01-11 15:54:04 +0000

    Ability to configure mail driver

--- app/init/swift.php
+++ app/init/swift.php
@@ -4,4 +4,22 @@
  * This source file is subject to the MIT license that is bundled with this package in the file LICENCE.md. 
  */
 
-return Swift_Mailer::newInstance(Swift_MailTransport::newInstance());
+$config = $app->config->mail;
+if ($config['driver'] == 'mail') {
+    $transport = Swift_MailTransport::newInstance();
+} else if ($config['driver'] == 'smtp') {
+    $transport = Swift_SmtpTransport::newInstance($config['host'], $config['port']);
+
+    if (isset($config['encryption'])) {
+        $transport->setEncryption($config['encryption']);
+    }
+    if (isset($config['user'])) {
+        $transport->setUsername($config['user']);
+    }
+    if (isset($config['password'])) {
+        $transport->setPassword($config['password']);
+    }
+    var_dump($transport);
+}
+
+return Swift_Mailer::newInstance($transport);
