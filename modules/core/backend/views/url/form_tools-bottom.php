9de29fc132a6e7db36b480580ca0d7f572253281
Jack Sleight <jacksleight@gmail.com>
2017-05-15 10:27:36 +0100

    Fix incorrect add URL button

--- modules/core/backend/views/url/form_tools-bottom.php
+++ modules/core/backend/views/url/form_tools-bottom.php
@@ -1,10 +1,12 @@
 <?php if (!$content->isNew()) { ?>
-    <li><a href="<?= $this->url([
-        'entity'    => $info->name,
-        'action'    => 'edit',
-    ], 'core_content', true) . $this->url->query([
-        'url' => 'mailto:',
-    ], true) ?>" class="btn btn-focus btn-out icon-add">
-        New <?= str_replace('URL', 'Email URL', $info->singular) ?>
-    </a></li>
+    <?php if (!isset($node)) { ?>
+        <li><a href="<?= $this->url([
+            'entity'    => $info->name,
+            'action'    => 'edit',
+        ], 'core_content', true) . $this->url->query([
+            'url' => 'mailto:',
+        ], true) ?>" class="btn btn-focus btn-out icon-add">
+            New <?= str_replace('URL', 'Email URL', $info->singular) ?>
+        </a></li>
+    <?php } ?>
 <?php } ?>
