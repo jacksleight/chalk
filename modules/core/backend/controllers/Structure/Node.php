dac1665a5326b7cca9ef0ac9a8a9c3b16ff091cc
Jack Sleight <jacksleight@gmail.com>
2018-09-10 15:21:26 +0100

    Fix duplicating home page

--- modules/core/backend/controllers/Structure/Node.php
+++ modules/core/backend/controllers/Structure/Node.php
@@ -148,7 +148,7 @@ class Node extends Entity
 
         $node = new StructureNode();
         $node->structure = $this->em('core_structure')->id($entity->structure->id);
-        $node->parent    = $this->em('core_structure_node')->id($entity->parent->id);
+        $node->parent    = isset($entity->parent) ? $this->em('core_structure_node')->id($entity->parent->id) : $entity;
         $node->content   = $content;
         $node->sort      = $entity->sort + 1;
         $content->nodes->add($node);
