6e021190e0545e0a02c15cf36081d961ac91c28a
Jack Sleight <jacksleight@gmail.com>
2019-01-21 11:29:04 +0000

    Versions bump

--- app/library/Chalk.php
+++ app/library/Chalk.php
@@ -14,7 +14,7 @@ use Coast\Response;
 
 class Chalk extends CoastApp
 {
-    const VERSION           = '0.7.7';
+    const VERSION           = '0.7.8';
 
     const STATUS_DRAFT      = 'draft';
     const STATUS_PENDING    = 'pending';
